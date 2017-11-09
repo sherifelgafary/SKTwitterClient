c13092e531fd45eacca1a504e2b81c33b8fffa9b
sherif-Khaled <sherifelgafary@gmail.com>
2017-11-09 04:04:18 +0200

    SK : adding project pod files

---
+++ SKTwitterClient/SKTwitterClient/ViewControllers/SplashScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenViewController.swift
+//  SKTwitterClient
+//
+//  Created by sherif Khaled on 11/9/17.
+//  Copyright © 2017 SherifKhaled. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
